7ff614095385dcdc0263d90482233b0d1b68f3c2
Paweł Sypek <paw3lsyp3k@gmail.com>
2021-10-22 16:23:40 +0200

    end of 3rd lesson

---
+++ src/data/users.js
@@ -0,0 +1,35 @@
+export const users = [
+    {
+        name: 'Adam Nijaki',
+        attendance: '45%',
+        average: '2.4',
+    },
+    {
+        name: 'Kasia Zabawna',
+        attendance: '25%',
+        average: '1.4',
+    },
+
+    {
+        name: 'Karol Palczak',
+        attendance: '85%',
+        average: '4.5',
+    },
+
+    {
+        name: 'Nikodem Zagrobelny',
+        attendance: '75%%',
+        average: '3.8',
+    },
+
+    {
+        name: 'Dagmara Szewaczyk',
+        attendance: '55%',
+        average: '2.6',
+    },
+    {
+        name: 'Kubuś Puchatek',
+        attendance: '80%',
+        average: '4.2',
+    },
+];
